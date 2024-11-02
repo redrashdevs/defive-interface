16da17569592e0850c62e6114af1273375947ebe
ArcaneTwisted <arcanetwisted@gmail.com>
2024-11-03 01:30:24 +0330

    chore: changed pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,5 +1,5 @@
 import cx from "classnames";
-
+import ReactPaginate from "react-paginate";
 import Button from "components/Button/Button";
 
 import "./Pagination.css";
@@ -11,55 +11,34 @@ type Props = {
   topMargin?: boolean;
 };
 
-function getPageNumbers(current, max = 1) {
-  if (max === 1) return [];
-  if (current === 1) {
-    return max >= 3 ? [1, 2, 3] : [1, 2];
-  } else if (current === max) {
-    return max >= 3 ? [current - 2, current - 1, current] : [current - 1, current];
-  } else {
-    return [current - 1, current, current + 1];
-  }
-}
 
 export default function Pagination({ page, pageCount, topMargin = true, onPageChange }: Props) {
   if (pageCount <= 1) {
     return <></>;
   }
 
-  const middleButtons = getPageNumbers(page, pageCount).map((pageNumber) => {
-    return (
-      <button
-        key={pageNumber}
-        className={"pagination-btn" + (pageNumber === page ? " active" : "")}
-        onClick={() => onPageChange(pageNumber)}
-      >
-        {pageNumber}
-      </button>
-    );
-  });
-
   return (
-    <div
-      className={cx("pagination", {
-        "mt-25": topMargin,
-      })}
-    >
-      <div className="pagination-buttons">
-        <Button variant="secondary" onClick={() => onPageChange(1)} disabled={page <= 1}>
-          {"|<"}
-        </Button>
-        <Button variant="secondary" onClick={() => onPageChange(page - 1)} disabled={page <= 1}>
-          {"<"}
-        </Button>
-        <div className="pagination-btn-middle">{middleButtons}</div>
-        <Button variant="secondary" onClick={() => onPageChange(page + 1)} disabled={page >= pageCount}>
-          {">"}
-        </Button>
-        <Button variant="secondary" onClick={() => onPageChange(pageCount)} disabled={page >= pageCount}>
-          {">|"}
-        </Button>
-      </div>
+    <div className="flex">
+      <ReactPaginate
+        breakLabel="..."
+        nextLabel={<img src="/images/arrow-narrow-right.svg" />}
+        onPageChange={(p) => onPageChange(p.selected + 1)}
+        pageRangeDisplayed={1}
+        marginPagesDisplayed={1}
+        pageCount={pageCount}
+        forcePage={page - 1}
+        previousLabel={<img src="/images/arrow-narrow-left.svg" />}
+        renderOnZeroPageCount={null}
+        pageLinkClassName="w-full h-full flex items-center justify-center text-center"
+        breakLinkClassName="w-full h-full flex items-center justify-center text-center"
+        previousLinkClassName="w-full h-full flex items-center justify-center text-center"
+        nextLinkClassName="w-full h-full flex items-center justify-center text-center"
+        activeClassName={"bg-[#242429] text-white"}
+        pageClassName="p-0 w-[48px] h-[40px] hover:bg-[#242429] flex items-center justify-center rounded-[4px] mx-4"
+        previousClassName="flex items-center justify-center w-[48px] h-[40px] hover:bg-[#242429] rounded-[4px] p-0 mr-4"
+        nextClassName="flex items-center justify-center w-[48px] h-[40px] hover:bg-[#242429] rounded-[4px] p-0 ml-4"
+        containerClassName="px-8 h-[48px] bg-[#09090A] flex items-center h-full border-[1px] border-solid rounded-[8px] border-[#24242980]"
+      />
     </div>
   );
 }
